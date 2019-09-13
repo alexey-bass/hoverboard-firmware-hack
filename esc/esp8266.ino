b9629d6ae59bd8610f49ea4f7f78e3e211ec214b
Alexey Bass <alexey.bass@gmail.com>
2019-09-13 22:48:51 +0300

    latest stable
    2 separate controllers
    3.3v used as in stable regulated, bad experience with 5.0v from source power.

--- esc/esp8266.ino
+++ esc/esp8266.ino
@@ -1,3 +1,4 @@
+
 #include "ESP8266WiFi.h"
 #include <SPI.h>
 #include <Wire.h>
@@ -6,7 +7,6 @@
 #include <Adafruit_MCP4725.h>
 
 bool DEBUG_CONSOLE  = false;
-bool HC_LED_ENABLED = true;
 
 // SCL GPIO5
 // SDA GPIO4
@@ -23,10 +23,18 @@ uint8_t HC_LED_OUTPUT_PIN  = D4;
 uint8_t HC_PEDAL_INPUT_PIN = D6;
 uint8_t HC_ANALOG_IN_PIN   = A0;
 
-int16_t  HC_PWM_OUTPUT_LEVEL_CURRENT   = 0; // can be negative, otherwise positive can overlap into max int
-uint16_t HC_PWM_OUTPUT_LEVEL_MAX       = 1000; // 100% duty cycle is 1023
-uint8_t  HC_PWM_OUTPUT_LEVEL_UP_STEP   = 20;
-uint8_t  HC_PWM_OUTPUT_LEVEL_DOWN_STEP = 50; // brake faster then accelerate
+float    HC_DAC_SUPPLY_VOLTAGE = 3.3; // set correct DAC VCC voltage
+uint16_t HC_DAC_VALUE_MAX = 4095; // 0x0FFF
+
+// diff real value measured and set, should be 0.0
+// measure actual on Vout with multimeter when MIN is set diff here
+float    HC_DAC_ACTUAL_VOLTAGE_CORRECTION = +0.000; 
+
+float HC_THROTTLE_OUTPUT_LEVEL_CURRENT = 0.000; // start voltage
+float HC_THROTTLE_OUTPUT_LEVEL_MIN     = 1.100;
+float HC_THROTTLE_OUTPUT_LEVEL_MAX     = 2.000;
+float HC_THROTTLE_OUTPUT_LEVEL_STEP_UP = 0.050;
+float HC_THROTTLE_OUTPUT_LEVEL_STEP_DN = 0.100; // brake faster then accelerate
 // ===== END OF CONFIG =====
 
 void displaySetup() {
@@ -42,7 +50,7 @@ void displayPrepare() {
   hcDisplay.clearDisplay();
 //  hcDisplay.display();
   hcDisplay.setCursor(0, 0);
-  hcDisplay.print("Hover-Car");
+//  hcDisplay.print("Hover-Car");
 }
 
 int adcInput = 0;
@@ -52,43 +60,56 @@ float MCP4725_reading;
 void displayUpdate() {
   displayPrepare();
   String pedalStatus = (HC_PEDAL_ENGAGED_CURRENT) ? "YES" : "NO";
-  hcDisplay.printf("\nDIR: %s", (true) ? "F=>" : "<=B");
-  hcDisplay.printf("\nPDL: %s", (HC_PEDAL_ENGAGED_CURRENT) ? "YES" : "NO");
-//  hcDisplay.printf("\nPWM: %d", HC_PWM_OUTPUT_LEVEL_CURRENT);
+  hcDisplay.printf("DIR: %s", (true) ? "F=>" : "<=B");
+  hcDisplay.printf("\nGAS: %s", (HC_PEDAL_ENGAGED_CURRENT) ? "YES" : "NO");
+  hcDisplay.printf("\nBRK: %s", "NO");
 
   adcInput = analogRead(HC_ANALOG_IN_PIN);
-  voltageIn = (adcInput * 3.3 ) / 1024.0;
-  hcDisplay.print("\nVop: ");
+  voltageIn = (adcInput * HC_DAC_SUPPLY_VOLTAGE) / 1023.0;
+  hcDisplay.print("\nVtg: ");
+  hcDisplay.print(HC_THROTTLE_OUTPUT_LEVEL_CURRENT, 3);
+  hcDisplay.print("\nVou: ");
   hcDisplay.print(voltageIn, 3);
   
-  hcDisplay.printf("\n\n%d", millis() / 100);
+  hcDisplay.printf("\n%d", millis() / 100);
 }
+
+void setDacVoltage(float target) {
+  target += HC_DAC_ACTUAL_VOLTAGE_CORRECTION;
+  hcDac.setVoltage(round((HC_DAC_VALUE_MAX * target) / HC_DAC_SUPPLY_VOLTAGE), false);
+} 
 
 void setup() {
   if (DEBUG_CONSOLE) {
     Serial.begin(9600);
   }
-  
-  WiFi.mode(WIFI_OFF);
-  WiFi.forceSleepBegin();
-  
-  if (HC_LED_ENABLED) {
-    pinMode(HC_LED_OUTPUT_PIN, OUTPUT);
-    digitalWrite(HC_LED_OUTPUT_PIN, HIGH); // LED off
-  }
-  
-  pinMode(HC_PEDAL_INPUT_PIN, INPUT_PULLUP);
-  
-  displaySetup();
-  displayUpdate();
 
+  // disable wifi
+  //WiFi.mode(WIFI_OFF);
+  //WiFi.forceSleepBegin();
+
+  // setup LED
+  pinMode(HC_LED_OUTPUT_PIN, OUTPUT);
+  digitalWrite(HC_LED_OUTPUT_PIN, HIGH); // LED set OFF
+
+  // set pedal input
+  pinMode(HC_PEDAL_INPUT_PIN, INPUT_PULLUP);
+
+  // setup display
+  displaySetup();
+
+  // setup DAC
   hcDac.begin(0x60);
-  hcDac.setVoltage(0, false);
+  setDacVoltage(HC_THROTTLE_OUTPUT_LEVEL_MIN);
+  HC_THROTTLE_OUTPUT_LEVEL_CURRENT = HC_THROTTLE_OUTPUT_LEVEL_MIN;
+
+  displayUpdate();
 }
 
 void loop() {
   HC_PEDAL_ENGAGED_CURRENT = (digitalRead(HC_PEDAL_INPUT_PIN) == LOW) ? true : false;
 
+  // if pedal state changed
   if (HC_PEDAL_ENGAGED_PREV != HC_PEDAL_ENGAGED_CURRENT) {
 
     if (DEBUG_CONSOLE) {
@@ -98,65 +119,52 @@ void loop() {
       Serial.print((HC_PEDAL_ENGAGED_PREV) ? "YES" : "NO");
       Serial.println();
     }
-    
-    switch (HC_PEDAL_ENGAGED_CURRENT) {
-      case true: // pedal pressed
-        if (HC_LED_ENABLED) {
-          digitalWrite(HC_LED_OUTPUT_PIN, LOW); // LED on
-        }
-        break;
-        
-      case false: // pulled up, no pedal
-        if (HC_LED_ENABLED) {
-          digitalWrite(HC_LED_OUTPUT_PIN, HIGH); // LED off
-        }
-        break;
-    }
 
+    if (HC_PEDAL_ENGAGED_CURRENT) {
+      digitalWrite(HC_LED_OUTPUT_PIN, LOW); // LED set ON
+    } else {
+      digitalWrite(HC_LED_OUTPUT_PIN, HIGH); // LED set OFF
+    }
+    
     HC_PEDAL_ENGAGED_PREV = HC_PEDAL_ENGAGED_CURRENT;
+    
     displayUpdate();
   }
 
-  if (HC_PEDAL_ENGAGED_CURRENT && HC_PWM_OUTPUT_LEVEL_CURRENT < HC_PWM_OUTPUT_LEVEL_MAX) {
-    HC_PWM_OUTPUT_LEVEL_CURRENT += HC_PWM_OUTPUT_LEVEL_UP_STEP;
-    if (HC_PWM_OUTPUT_LEVEL_CURRENT > HC_PWM_OUTPUT_LEVEL_MAX) { // if exeed MAX
-      HC_PWM_OUTPUT_LEVEL_CURRENT = HC_PWM_OUTPUT_LEVEL_MAX;
+  if (HC_PEDAL_ENGAGED_CURRENT && HC_THROTTLE_OUTPUT_LEVEL_CURRENT < HC_THROTTLE_OUTPUT_LEVEL_MAX) {
+    HC_THROTTLE_OUTPUT_LEVEL_CURRENT += HC_THROTTLE_OUTPUT_LEVEL_STEP_UP;
+    if (HC_THROTTLE_OUTPUT_LEVEL_CURRENT > HC_THROTTLE_OUTPUT_LEVEL_MAX) { // if exeed MAX
+      HC_THROTTLE_OUTPUT_LEVEL_CURRENT = HC_THROTTLE_OUTPUT_LEVEL_MAX;
     }
     
+    setDacVoltage(HC_THROTTLE_OUTPUT_LEVEL_CURRENT);
     if (DEBUG_CONSOLE) {
-      Serial.printf("PWM: %d" + HC_PWM_OUTPUT_LEVEL_CURRENT);
-      Serial.println();
-    }
-
-    hcDac.setVoltage(HC_PWM_OUTPUT_LEVEL_CURRENT, false);
-    if (DEBUG_CONSOLE) {
-      MCP4725_reading = (3.3/4096.0) * HC_PWM_OUTPUT_LEVEL_CURRENT; //3.3 is your supply voltage
+      //MCP4725_reading = (HC_DAC_SUPPLY_VOLTAGE / 4096.0) * HC_THROTTLE_OUTPUT_LEVEL_CURRENT; //3.3 is your supply voltage
       Serial.print("Expected Voltage: ");
-      Serial.println(MCP4725_reading, 3);
+      Serial.println(HC_THROTTLE_OUTPUT_LEVEL_CURRENT);
+      //Serial.print("Current Voltage: ");
+      //Serial.println(voltageIn);
+      //Serial.println(MCP4725_reading, 3);
     }
     
     displayUpdate();
     
-  } else if (!HC_PEDAL_ENGAGED_CURRENT && HC_PWM_OUTPUT_LEVEL_CURRENT > 0) {
-    HC_PWM_OUTPUT_LEVEL_CURRENT -= HC_PWM_OUTPUT_LEVEL_DOWN_STEP;
-    if (HC_PWM_OUTPUT_LEVEL_CURRENT < 0) { // if we went negative
-      HC_PWM_OUTPUT_LEVEL_CURRENT = 0;
+  } else if (!HC_PEDAL_ENGAGED_CURRENT && HC_THROTTLE_OUTPUT_LEVEL_CURRENT > HC_THROTTLE_OUTPUT_LEVEL_MIN) {
+    HC_THROTTLE_OUTPUT_LEVEL_CURRENT -= HC_THROTTLE_OUTPUT_LEVEL_STEP_DN;
+    if (HC_THROTTLE_OUTPUT_LEVEL_CURRENT < HC_THROTTLE_OUTPUT_LEVEL_MIN) { // if we went negative
+      HC_THROTTLE_OUTPUT_LEVEL_CURRENT = HC_THROTTLE_OUTPUT_LEVEL_MIN;
     }
     
+    setDacVoltage(HC_THROTTLE_OUTPUT_LEVEL_CURRENT);
     if (DEBUG_CONSOLE) {
-      Serial.printf("PWM: %d" + HC_PWM_OUTPUT_LEVEL_CURRENT);
-      Serial.println();
-    }
-
-    hcDac.setVoltage(HC_PWM_OUTPUT_LEVEL_CURRENT, false);
-    if (DEBUG_CONSOLE) {
-      MCP4725_reading = (3.3/4096.0) * HC_PWM_OUTPUT_LEVEL_CURRENT; //3.3 is your supply voltage
+      //MCP4725_reading = (3.3/4096.0) * HC_THROTTLE_OUTPUT_LEVEL_CURRENT; //3.3 is your supply voltage
       Serial.print("Expected Voltage: ");
-      Serial.println(MCP4725_reading, 3);
+      Serial.println(HC_THROTTLE_OUTPUT_LEVEL_CURRENT);
+      //Serial.print("Current Voltage: ");
+      //Serial.println(voltageIn);
+      //Serial.println(MCP4725_reading, 3);
     }
-    
 
-    
     displayUpdate();
   }
 
